2ee6eb2ae71fe9310f8e8777a4042d3d50795c34
jharry444 <jharrison1@qa.com>
2024-07-25 13:39:30 +0100

    Prep for ex. 2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,20 +5,12 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
     return (
         <Navbar bg="light" expand="lg">
-            {/*
-                Wrap a Link to / around the Navbar.Brand
-                Give it a className of nav-link
-            */}
             <Navbar.Brand>OMDB Example</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    {/* Add a link to /search with a class name of nav-link */}
+                    <Link to="/search">Search</Link>
                 </Nav>
-                {/* <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form> */}
             </Navbar.Collapse>
         </Navbar>
     );
