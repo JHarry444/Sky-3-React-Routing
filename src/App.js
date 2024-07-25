156582f979c1055ff011335fdd58190a4adc91e9
jharry444 <jharrison1@qa.com>
2024-07-25 13:39:12 +0100

    prep fro ex. 2

--- src/App.js
+++ src/App.js
@@ -9,14 +9,9 @@ import Details from './components/Details';
 const App = () => {
   return (
     <Router>
-      {/* Add a Router */}
-      {/* Add Navigation */}
       <Navigation />
-      {/* Create a routes element */}
       <Routes>
-        {/* Add a route to the Routes with a path of '/' and an element of Home */}
         <Route path="/" element={<Home />} />
-        {/* Add another route for Search at /search */}
         <Route path="/search" element={<Search />} />
         {/* Add a route for /details/:id that renders the details component */}
       </Routes>
